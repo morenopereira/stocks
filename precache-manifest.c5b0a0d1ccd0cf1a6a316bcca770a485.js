b17dadf2244db3ff98e952d4302be8af9b813b95
Moreno Andrade <moreno@flieber.com>
2019-11-12 00:16:35 -0300

    Updates

---
+++ precache-manifest.c5b0a0d1ccd0cf1a6a316bcca770a485.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f7d6996d9a8bdbff009fd1a6122fa21",
+    "url": "/stocks/index.html"
+  },
+  {
+    "revision": "ba199cf84b789924e65e",
+    "url": "/stocks/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "6d79b2b0fd8b4151633c",
+    "url": "/stocks/static/css/main.132e896e.chunk.css"
+  },
+  {
+    "revision": "ba199cf84b789924e65e",
+    "url": "/stocks/static/js/2.cde641dd.chunk.js"
+  },
+  {
+    "revision": "6d79b2b0fd8b4151633c",
+    "url": "/stocks/static/js/main.1990b765.chunk.js"
+  },
+  {
+    "revision": "4fcf16532934b26a5e71",
+    "url": "/stocks/static/js/runtime-main.86bb2ed3.js"
+  }
+]);
